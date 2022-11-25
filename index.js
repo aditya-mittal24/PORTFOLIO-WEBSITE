628ac02118a495d0d6a14e5ad26587f15fd5a0cd
Aditya <aditya@Adityas-MacBook-Air-2.local>
2022-11-25 22:09:42 +0530

    updated nov-22

--- index.js
+++ index.js
@@ -13,4 +13,32 @@ if (navigator.userAgent.match(/Mobile/)){
   $(".navbar").css("background-color", "transparent");
   $(".navbar").css("display", "block");
   $(".navbar-brand").css("display", "none");
+  $(".right").attr("data-aos","false");
 }
+
+
+// $(".project1").hover(function(){
+//   $(".project1 hr").animate({width: "100%"});
+//   $(".project1 .card-text").animate({left:"0"});
+// });
+// $(".project2").hover(function(){
+//   $(".project2 hr").animate({width: "100%"});
+//   $(".project2 .card-text").animate({left:"0"});
+// });
+// $(".project3").hover(function(){
+//   $(".project3 hr").animate({width: "100%"});
+//   $(".project3 .card-text").animate({left:"0"});
+// });
+// $(".project4").hover(function(){
+//   $(".project4 hr").animate({width: "100%"});
+//   $(".project4 .card-text").animate({left:"0"});
+// });
+
+// $(".card-img-overlay").on("mouseleave",function(){
+//   $("hr").css("width","100");
+//   $(".card-text").css("left","80%");
+// });
+
+// $(".card").on("mouseleave",function(){
+//   $(".card-img-overlay").css("visibility", "hidden");
+// });
